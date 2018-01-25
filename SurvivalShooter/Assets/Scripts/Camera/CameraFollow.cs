e082f80b4c158506e79e58c46eed9870defd5b0f
Alejandro Zepeda <zepedaalex25@gmail.com>
2018-01-24 19:11:57 -0800

    Adding project to repo

---
+++ SurvivalShooter/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public float smoothing = 5f;
+    public int screenshotNum = 0;
+    Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - target.position;
+	}
+	
+	// Using fixed update since player is moving using fixed update
+	void FixedUpdate () {
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+
+        if(Input.GetKeyDown("e")) {
+            ScreenCapture.CaptureScreenshot("Screenshot" + screenshotNum + ".png");
+            screenshotNum++;
+        }
+    }
+
+}
